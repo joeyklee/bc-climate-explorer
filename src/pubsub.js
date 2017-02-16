6a4278f92b332e72a4ec5e0a3f2a8d9fd130acec
Kevin Klugmann <kevin.klugmann@web.de>
2017-02-16 13:41:58 +0100

    add pubsub module

---
+++ src/pubsub.js
@@ -0,0 +1,30 @@
+// Code from https://gist.github.com/learncodeacademy/777349747d8382bfb722
+
+module.exports = {
+  events: {},
+  keys: {
+    exampleEvent: "exampleEvent"
+  },
+  on: function (eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+  off: function(eventName, fn) {
+    if (this.events[eventName]) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      };
+    }
+  },
+  emit: function (eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+    console.log(this.events);
+  }
+};
