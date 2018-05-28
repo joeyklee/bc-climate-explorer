ba8e5587c841b9886a69e5fac997ee8e2bd0d09d
Kevin Klugmann <kevin.klugmann@web.de>
2018-05-28 19:40:51 +0200

    #24: replace try and catch in setup.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,134 +1,136 @@
 // Styles
-import '../stylesheets/scss/main.scss'
+import "../stylesheets/scss/main.scss"
+
+// Bc-climate-explorer files
+import "./setup";
 
 var app = app || {};
 
-app.main = (function() {
-		console.log("hello from index")
+app.main = (function () {
+    console.log("hello from index")
     // store variables in el and expose them to other js files
     let el = {
-    	focalUnitA:'SBSdw3',
-    	focalUnitB:'BGxh2',
-    	x:{
-    		timescale:'Annual',
-    		variable:'MAT',
-    		scatterplot:{
-    			zones:[],
-    			data:[]
-    		},
-    		timeseries:{
-    			a:[],
-                a_rcp45:[],
-                a_rcp85:[],
-    			b:[],
-                b_rcp45:[],
-                b_rcp85:[],
-                years:[],
-                years_projected:[]
-    		}
-    	},
-    	y:{
-    		timescale:'Annual',
-    		variable:'MAP',
-    		scatterplot:{
-    			zones:[],
-    			data:[]
-    		},
-    		timeseries:{
-    			a:[],
-                a_rcp45:[],
-                a_rcp85:[],
-    			b:[],
-                b_rcp45:[],
-                b_rcp85:[],
-                years:[],
-                years_projected:[]
-    		}
-    	},
-    	geo: {
-    		basemap:{
-    			satellite:"satellite-streets-v10",
-    			light:"light-v9"
-    		}
-    	},
-    	selectors:{},
-    	colors:{},
-    	helpers:{
-    		months: [
-    		  { "month": "January", "number": "01" },
-    		  { "month": "February", "number": "02" },
-    		  { "month": "March", "number": "03" },
-    		  { "month": "April", "number": "04" },
-    		  { "month": "May", "number": "05" },
-    		  { "month": "June", "number": "06" },
-    		  { "month": "July", "number": "07" },
-    		  { "month": "August", "number": "08" },
-    		  { "month": "September", "number": "09" },
-    		  { "month": "October", "number": "10" },
-    		  { "month": "November", "number": "11" },
-    		  { "month": "December", "number": "12" }
-    		],
-    		seasons:[
-    		  { "season": "Winter", "abbv": "wt" },
-    		  { "season": "Fall", "abbv": "at" },
-    		  { "season": "Spring", "abbv": "sp" },
-    		  { "season": "Summer", "abbv": "sm" }
-    		],
-    		chartLayout: {
-    		    plot_bgcolor: '#FFFFFF',
-    		    paper_bgcolor: '#FFFFFF',
-    		    margin: {
-    		      l: 50,
-    		      r: 20,
-    		      b: 40,
-    		      t: 30,
-    		      pad: 0
-    		    },
-    		    xaxis: {
-    		      autotick: true,
-    		      showgrid: false,
-    		      ticks: "inside",
-    		      color: "#000000",
-    		      autotick: true,
-    		    },
-    		    yaxis: {
-    		      showticklabels: true,
-    		      autotick: true,
-    		      showgrid: false,
-    		      ticks: "inside",
-    		      color: "#000000",
-    		      zerolinewidth: 1,
-    		      zeroline: true
-    		    }
-    		  }
-    	}
+        focalUnitA: 'SBSdw3',
+        focalUnitB: 'BGxh2',
+        x: {
+            timescale: 'Annual',
+            variable: 'MAT',
+            scatterplot: {
+                zones: [],
+                data: []
+            },
+            timeseries: {
+                a: [],
+                a_rcp45: [],
+                a_rcp85: [],
+                b: [],
+                b_rcp45: [],
+                b_rcp85: [],
+                years: [],
+                years_projected: []
+            }
+        },
+        y: {
+            timescale: 'Annual',
+            variable: 'MAP',
+            scatterplot: {
+                zones: [],
+                data: []
+            },
+            timeseries: {
+                a: [],
+                a_rcp45: [],
+                a_rcp85: [],
+                b: [],
+                b_rcp45: [],
+                b_rcp85: [],
+                years: [],
+                years_projected: []
+            }
+        },
+        geo: {
+            basemap: {
+                satellite: "satellite-streets-v10",
+                light: "light-v9"
+            }
+        },
+        selectors: {},
+        colors: {},
+        helpers: {
+            months: [
+                {"month": "January", "number": "01"},
+                {"month": "February", "number": "02"},
+                {"month": "March", "number": "03"},
+                {"month": "April", "number": "04"},
+                {"month": "May", "number": "05"},
+                {"month": "June", "number": "06"},
+                {"month": "July", "number": "07"},
+                {"month": "August", "number": "08"},
+                {"month": "September", "number": "09"},
+                {"month": "October", "number": "10"},
+                {"month": "November", "number": "11"},
+                {"month": "December", "number": "12"}
+            ],
+            seasons: [
+                {"season": "Winter", "abbv": "wt"},
+                {"season": "Fall", "abbv": "at"},
+                {"season": "Spring", "abbv": "sp"},
+                {"season": "Summer", "abbv": "sm"}
+            ],
+            chartLayout: {
+                plot_bgcolor: '#FFFFFF',
+                paper_bgcolor: '#FFFFFF',
+                margin: {
+                    l: 50,
+                    r: 20,
+                    b: 40,
+                    t: 30,
+                    pad: 0
+                },
+                xaxis: {
+                    autotick: true,
+                    showgrid: false,
+                    ticks: "inside",
+                    color: "#000000",
+                    autotick: true,
+                },
+                yaxis: {
+                    showticklabels: true,
+                    autotick: true,
+                    showgrid: false,
+                    ticks: "inside",
+                    color: "#000000",
+                    zerolinewidth: 1,
+                    zeroline: true
+                }
+            }
+        }
     };
 
 
+    let formatClimateName = function (climateVar, stateTime) {
+        let climate_selected = null;
+        let timevar;
+        let specials = ["DD_0", "DD0", "DD_18", "DD18", "DD5", "DD_18"];
 
-    let formatClimateName = function(climateVar, stateTime) {
-      let climate_selected = null;
-      let timevar;
-      let specials = ["DD_0", "DD0", "DD_18", "DD18", "DD5", "DD_18"];
-
-      if (stateTime.toLowerCase() == 'annual') {
-        climate_selected = climateVar;
-      } else if (el.helpers.seasons.filter(i => (i.season === stateTime)).length > 0) {
-        timevar = el.helpers.seasons.filter(i => (i.season === stateTime))[0].abbv
-        climate_selected = climateVar + '_' + timevar; // for seasonal variables
-      } else {
-        timevar = el.helpers.months.filter(i => (i.month === stateTime))[0].number
-
-        // climateVar.startsWith("DD_0") || climateVar.startsWith("DD_18")
-        if (specials.includes(climateVar) === true) {
-          climate_selected = climateVar + "_" + timevar; // for jan - dec  
+        if (stateTime.toLowerCase() == 'annual') {
+            climate_selected = climateVar;
+        } else if (el.helpers.seasons.filter(i => (i.season === stateTime)).length > 0) {
+            timevar = el.helpers.seasons.filter(i => (i.season === stateTime))[0].abbv
+            climate_selected = climateVar + '_' + timevar; // for seasonal variables
         } else {
-          climate_selected = climateVar + timevar; // for jan - dec  
+            timevar = el.helpers.months.filter(i => (i.month === stateTime))[0].number
+
+            // climateVar.startsWith("DD_0") || climateVar.startsWith("DD_18")
+            if (specials.includes(climateVar) === true) {
+                climate_selected = climateVar + "_" + timevar; // for jan - dec
+            } else {
+                climate_selected = climateVar + timevar; // for jan - dec
+            }
+
         }
 
-      }
-    
-      return climate_selected.toLowerCase()
+        return climate_selected.toLowerCase()
     }
 
     return {
@@ -140,16 +142,16 @@ app.main = (function() {
 })();
 
 // call app.map.init() once the DOM is loaded
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     // app.<modulename>.init();
     app.setup.init()
-    	.then((data) => {
+        .then((data) => {
             console.log(data);
-    		app.controllers.init();
-    		app.geo.init();
-    		app.scatterplot.init();
+            app.controllers.init();
+            app.geo.init();
+            app.scatterplot.init();
             app.timeseries.init();
-    	});
-    
-    
+        });
+
+
 });
--- src/js/setup.js
+++ src/js/setup.js
@@ -1,199 +1,192 @@
 var app = app || {};
 
-app.setup = (function() {
-  console.log("hello from setup")
-  var el = null;
+app.setup = (function () {
+    console.log("hello from setup")
+    var el = null;
 
-  /***
-  * @ Load Climate Variables
-  * @ Loads the climate normal variables from a json and appends them to the page
-  **/
-  async function loadClimateVariables() {
-    try{
-      let data = await $.getJSON("src/data/climate-variables-master/climate-variables-list.json")
-      let dropdownMenu = createClimateVariablesDropdown(data);
+    /***
+     * @ Load Climate Variables
+     * @ Loads the climate normal variables from a json and appends them to the page
+     **/
+    async function loadClimateVariables() {
+        let data = await $.getJSON("src/data/climate-variables-master/climate-variables-list.json", () => {
+            let dropdownMenu = createClimateVariablesDropdown(data);
 
-      $("#X-Variable-Dropdown").append(dropdownMenu);
-      $("#Y-Variable-Dropdown").append(dropdownMenu);
-    } catch {
-      console.log("no climate-variables-list found")
+            $("#X-Variable-Dropdown").append(dropdownMenu);
+            $("#Y-Variable-Dropdown").append(dropdownMenu);
+        }).fail(() => {
+            console.log("no climate-variables-list found")
+        });
+
     }
-    
-  }
 
-  function createClimateVariablesDropdown(json) {
-    let dropdownOptionsList = [];
+    function createClimateVariablesDropdown(json) {
+        let dropdownOptionsList = [];
 
-    json.forEach(function(d) {
-      let dropdownOption = `<option label="${d.timeScale}" data-timescale="${d.timeScale}" data-logtransform="${d.logTransform}" value="${d.variable}">${d.variable} - ${d.description}</option>`
-      dropdownOptionsList.push(dropdownOption)
-    })
+        json.forEach(function (d) {
+            let dropdownOption = `<option label="${d.timeScale}" data-timescale="${d.timeScale}" data-logtransform="${d.logTransform}" value="${d.variable}">${d.variable} - ${d.description}</option>`
+            dropdownOptionsList.push(dropdownOption)
+        })
 
 
-    let dropdown = `
+        let dropdown = `
                 <select data-placeholder="Climate Variable" class="chosen-select dropdown" tabindex="2">
                   <option value=""></option>
                   ${dropdownOptionsList.join("\n")}
                 </select>
         `
-    return dropdown;
-  }
-
-
-
-  /***
-  * @ Load timescales
-  * @ Loads the timescale units from a json and appends them to the page
-  * @ */
-  async function loadTimescales() {
-    try{
-      let data = await $.getJSON("src/data/timescale-list/timescale-list.json")
-      let dropdownMenu = createTimescaleDropdown(data);
-      
-      $("#X-Time-Dropdown").append(dropdownMenu);
-      $("#Y-Time-Dropdown").append(dropdownMenu);
-    } catch {
-      console.log("no timescale-list found")
+        return dropdown;
     }
-    
-  }
-
-  function createTimescaleDropdown(json) {
-    let dropdownOptionsList = [];
-
-    json.forEach(function(d) {
-      let dropdownOption = `<option value="${d.timeUnit}" data-timescale="${d.timeScale}">${d.timeUnit}</option>`
-      dropdownOptionsList.push(dropdownOption)
-    })
 
 
-    let dropdown = `
+    /***
+     * @ Load timescales
+     * @ Loads the timescale units from a json and appends them to the page
+     * @ */
+    async function loadTimescales() {
+        let data = await $.getJSON("src/data/timescale-list/timescale-list.json", () => {
+            let dropdownMenu = createTimescaleDropdown(data);
+
+            $("#X-Time-Dropdown").append(dropdownMenu);
+            $("#Y-Time-Dropdown").append(dropdownMenu);
+        }).fail(() => {
+            console.log("no timescale-list found");
+        });
+    }
+
+    function createTimescaleDropdown(json) {
+        let dropdownOptionsList = [];
+
+        json.forEach(function (d) {
+            let dropdownOption = `<option value="${d.timeUnit}" data-timescale="${d.timeScale}">${d.timeUnit}</option>`
+            dropdownOptionsList.push(dropdownOption)
+        })
+
+
+        let dropdown = `
                 <select data-placeholder="Time" class="chosen-select dropdown" tabindex="2">
                   <option value=""></option>
                   ${dropdownOptionsList.join("\n")}
                 </select>
         `
-    return dropdown;
-  }
-
-
-  /***
-  * @ Load Focal Units
-  * @ Loads the focal units from a json and appends them to the page
-  */
-  async function loadFocalUnits() {
-    
-    try{
-      let data = await $.getJSON("src/data/bec-names-list/BGCunits_Ver10_2017.json")
-      let dropdownMenu = createFocalUnitDropdown(data);
-      $("#Focal-Unit-A-Selector").append(dropdownMenu);
-      $("#Focal-Unit-B-Selector").append(dropdownMenu);
-    } catch{
-      console.log("no focal units found")
+        return dropdown;
     }
-    
-  }
 
-  function createFocalUnitDropdown(json) {
-    let dropdownOptionsList = [];
 
-    json.forEach(function(d) {
-      let dropdownOption = `<option value="${d.BGC_NoSpace}">${d.BGC_NoSpace} - ${d.SubzVarPh_Description}</option>`
-      dropdownOptionsList.push(dropdownOption)
-    })
+    /***
+     * @ Load Focal Units
+     * @ Loads the focal units from a json and appends them to the page
+     */
+    async function loadFocalUnits() {
+        let data = await $.getJSON("src/data/bec-names-list/BGCunits_Ver10_2017.json", () => {
+            let dropdownMenu = createFocalUnitDropdown(data);
 
-    let dropdown = `
+            $("#Focal-Unit-A-Selector").append(dropdownMenu);
+            $("#Focal-Unit-B-Selector").append(dropdownMenu);
+        }).fail(() => {
+            console.log("no focal units found")
+        });
+    }
+
+    function createFocalUnitDropdown(json) {
+        let dropdownOptionsList = [];
+
+        json.forEach(function (d) {
+            let dropdownOption = `<option value="${d.BGC_NoSpace}">${d.BGC_NoSpace} - ${d.SubzVarPh_Description}</option>`
+            dropdownOptionsList.push(dropdownOption)
+        })
+
+        let dropdown = `
                 <select data-placeholder="BEC Zone" class="chosen-select dropdown" tabindex="2">
                   <option value=""></option>
                   ${dropdownOptionsList.join("\n")}
                 </select>
         `
-    return dropdown;
-  }
+        return dropdown;
+    }
 
 
-  /**
-  * @ Get all the selectors
-  * @ #Controller, #Charts
-  * @
-  */
-  function loadSelectors(){
-    let ControllerSelectors, ChartSelectors;
-    // Get DOM elements
-    ControllerSelectors = $("#Controller")
-    ChartSelectors = $("#Charts")
+    /**
+     * @ Get all the selectors
+     * @ #Controller, #Charts
+     * @
+     */
+    function loadSelectors() {
+        let ControllerSelectors, ChartSelectors;
+        // Get DOM elements
+        ControllerSelectors = $("#Controller")
+        ChartSelectors = $("#Charts")
 
-    // focal unit selectors
-    el.selectors.focalUnitA = ControllerSelectors.find("#Focal-Unit-A-Selector select")
-    el.selectors.focalUnitB = ControllerSelectors.find("#Focal-Unit-B-Selector select")
-    // time component selectors
-    el.selectors.xTimescale = ControllerSelectors.find("#X-Time-Dropdown select")
-    el.selectors.yTimescale = ControllerSelectors.find("#Y-Time-Dropdown select")
-    // variable selectors
-    el.selectors.xVariable = ControllerSelectors.find("#X-Variable-Dropdown select")
-    el.selectors.yVariable = ControllerSelectors.find("#Y-Variable-Dropdown select")
+        // focal unit selectors
+        el.selectors.focalUnitA = ControllerSelectors.find("#Focal-Unit-A-Selector select")
+        el.selectors.focalUnitB = ControllerSelectors.find("#Focal-Unit-B-Selector select")
+        // time component selectors
+        el.selectors.xTimescale = ControllerSelectors.find("#X-Time-Dropdown select")
+        el.selectors.yTimescale = ControllerSelectors.find("#Y-Time-Dropdown select")
+        // variable selectors
+        el.selectors.xVariable = ControllerSelectors.find("#X-Variable-Dropdown select")
+        el.selectors.yVariable = ControllerSelectors.find("#Y-Variable-Dropdown select")
 
-    // geo controllers
-    el.selectors.geoZone = ChartSelectors.find("#Geo-Zone-Button")
-    el.selectors.geoUnit = ChartSelectors.find("#Geo-Unit-Button")
-    el.selectors.geoX = ChartSelectors.find("#Geo-X-Button")
-    el.selectors.geoY = ChartSelectors.find("#Geo-Y-Button")
-    el.selectors.basemap = ChartSelectors.find(".map-basemap-switcher")
-    el.selectors.geoMenu = ChartSelectors.find("#Geo-Menu")
+        // geo controllers
+        el.selectors.geoZone = ChartSelectors.find("#Geo-Zone-Button")
+        el.selectors.geoUnit = ChartSelectors.find("#Geo-Unit-Button")
+        el.selectors.geoX = ChartSelectors.find("#Geo-X-Button")
+        el.selectors.geoY = ChartSelectors.find("#Geo-Y-Button")
+        el.selectors.basemap = ChartSelectors.find(".map-basemap-switcher")
+        el.selectors.geoMenu = ChartSelectors.find("#Geo-Menu")
 
-    // geopopup
-    // el.selectors.geoPopup = $("#geo-popup")
-    el.selectors.geoPopupSelectA = null;
-    el.selectors.geoPopupSelectB = null;
+        // geopopup
+        // el.selectors.geoPopup = $("#geo-popup")
+        el.selectors.geoPopupSelectA = null;
+        el.selectors.geoPopupSelectB = null;
 
-    // return a promise in order to use chaining
-    return new Promise( (resolve, reject) => {
-      resolve(true)
-    })
+        // return a promise in order to use chaining
+        return new Promise((resolve, reject) => {
+            resolve(true)
+        })
 
-  }
+    }
 
-  function initChosen(){
-    $(".chosen-select").chosen()
-    return new Promise( (resolve, reject) => {
-      resolve(true)
-    })
-  }
+    function initChosen() {
+        $(".chosen-select").chosen()
+        return new Promise((resolve, reject) => {
+            resolve(true)
+        })
+    }
 
-  function toggleAbout(){
-    $("#About-button, #About-close").click(function(){
-      console.log("about clicked!")
-      $("#About").toggleClass("active")
-    })
-  };
+    function toggleAbout() {
+        $("#About-button, #About-close").click(function () {
+            console.log("about clicked!")
+            $("#About").toggleClass("active")
+        })
+    };
 
-  function toggleHelp(){
-    $("#Help-button").click(function(){
-      $("#Help").toggleClass("active")
-    })
-  };
+    function toggleHelp() {
+        $("#Help-button").click(function () {
+            $("#Help").toggleClass("active")
+        })
+    };
 
 
-  var init = function() {
-    el = app.main.el;
-    toggleAbout()
-    toggleHelp()
-    // Load up all the components
-    return loadTimescales()
-      .then(loadClimateVariables)
-        .then(loadFocalUnits)
-        .then(loadSelectors)
-        .then(initChosen)
-  };
+    var init = function () {
+        el = app.main.el;
+        toggleAbout()
+        toggleHelp()
+        // Load up all the components
+        return loadTimescales()
+            .then(loadClimateVariables)
+            .then(loadFocalUnits)
+            .then(loadSelectors)
+            .then(initChosen)
+    };
 
-  // TODO: Return a promise
-  // to make sure everything
-  // is loaded, then continue
-  // runnning everything else
-  return {
-    init: init
-  }
-
+    // TODO: Return a promise
+    // to make sure everything
+    // is loaded, then continue
+    // runnning everything else
+    return {
+        init: init
+    }
 
 
 })();
